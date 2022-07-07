69a6e64f5a173023098258b2613f6b8e36aaa416
weiyafei <2553241022@qq.com>
2022-07-07 20:56:12 +0800

    parseTag

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -2,12 +2,15 @@
  * @Description:
  * @Author: wsy
  * @Date: 2022-07-07 11:26:33
- * @LastEditTime: 2022-07-07 14:49:20
+ * @LastEditTime: 2022-07-07 20:54:47
  * @LastEditors: wsy
  */
 
 import { NodeTypes } from './ast';
-
+const enum TagType {
+  Start,
+  End,
+}
 export function baseParse(content: string) {
   const context = createParserContext(content);
   return createRoot(parseChildren(context));
@@ -15,12 +18,39 @@ export function baseParse(content: string) {
 function parseChildren(context: any) {
   const nodes = [];
   let node;
-  if (context.source.startsWith('{{')) {
+  const s = context.source;
+  if (s.startsWith('{{')) {
     node = parseInterpolation(context);
+  } else if (s.startsWith('<')) {
+    if (/[a-z]/i.test(s[1])) {
+      console.log('parse element');
+      node = parseElement(context);
+    }
   }
-
   nodes.push(node);
   return nodes;
+}
+function parseElement(context: any) {
+  /**
+   * 1. 解析tag
+   * 2. 删除处理完成的代码
+   */
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End);
+  return element;
+}
+function parseTag(context: any, TagType: any) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+  if (TagType === TagType.End) {
+    return;
+  }
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
 function parseInterpolation(context: any) {
   //{{message}}
--- src/compiler-core/test/parse.spec.ts
+++ src/compiler-core/test/parse.spec.ts
@@ -5,10 +5,10 @@ import { NodeTypes } from '../src/ast';
  * @Description:
  * @Author: wsy
  * @Date: 2022-07-07 11:16:52
- * @LastEditTime: 2022-07-07 14:49:00
+ * @LastEditTime: 2022-07-07 20:20:59
  * @LastEditors: wsy
  */
-describe.only('Parse', () => {
+describe('Parse', () => {
   it('simple interpolation', () => {
     const ast = baseParse('{{ message  }}');
     expect(ast.children[0]).toStrictEqual({
@@ -17,6 +17,16 @@ describe.only('Parse', () => {
         type: NodeTypes.SIMPLE_EXPRESSION,
         content: 'message',
       },
+    });
+  });
+});
+
+describe('Element', () => {
+  it('simple element div', () => {
+    const ast = baseParse('<div></div>');
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: 'div',
     });
   });
 });
