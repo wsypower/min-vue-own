f4dd529ad57894c78076db66d81c8fb2b2e8e3b5
weiyafei <2553241022@qq.com>
2022-07-12 22:32:09 +0800

    transformExpression

--- src/compiler-core/test/codegen.spec.ts
+++ src/compiler-core/test/codegen.spec.ts
@@ -9,7 +9,7 @@ import { transformText } from '../src/transforms/transformText';
  * @Description:
  * @Author: wsy
  * @Date: 2022-07-11 00:21:29
- * @LastEditTime: 2022-07-12 22:22:34
+ * @LastEditTime: 2022-07-12 22:27:41
  * @LastEditors: wsy
  */
 describe('codegen', () => {
@@ -30,7 +30,7 @@ describe('codegen', () => {
   it('element', () => {
     const ast: any = baseParse('<div>hi,{{message}}</div>');
     transform(ast, {
-      nodeTransforms: [transformText, transformElement],
+      nodeTransforms: [transformExpression, transformText, transformElement],
     });
     const { code } = generate(ast);
     expect(code).toMatchSnapshot();
